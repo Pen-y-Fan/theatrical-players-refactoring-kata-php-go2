a7ac672c1b6442ee310e7bc68821b980bb7b3a9d
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-07-26 18:17:39 +0100

    Split to own Class (file)

---
+++ php/src/CreateStatementData.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theatrical;
+
+use Error;
+use stdClass;
+
+class CreateStatementData
+{
+    public function createStatementData($invoice, $plays): stdClass
+    {
+        $statementData = new stdClass();
+        $statementData->customer = $invoice->customer;
+        $statementData->performances = array_map(function (Performance $performance) use ($plays) {
+            $result = clone $performance;
+            $result->play = clone $this->playFor($result, $plays);
+            $result->amount = $this->amountFor($result);
+            $result->volumeCredits = $this->volumeCreditsFor($result);
+            return $result;
+        }, $invoice->performances);
+        $statementData->totalAmount = $this->totalAmount($statementData);
+        $statementData->totalVolumeCredits = $this->totalVolumeCredits($statementData);
+
+        return $statementData;
+    }
+
+    protected function amountFor(Performance $performance): int
+    {
+        switch ($performance->play->type) {
+            case 'tragedy':
+                $result = 40000;
+                if ($performance->audience > 30) {
+                    $result += 1000 * ($performance->audience - 30);
+                }
+                break;
+
+            case 'comedy':
+                $result = 30000;
+                if ($performance->audience > 20) {
+                    $result += 10000 + 500 * ($performance->audience - 20);
+                }
+                $result += 300 * $performance->audience;
+                break;
+
+            default:
+                throw new Error("Unknown type: {$performance->play->type}");
+        }
+        return $result;
+    }
+
+    private function playFor(Performance $performance, array $plays): Play
+    {
+        return $plays[$performance->playID];
+    }
+
+    public function volumeCreditsFor(Performance $performance): int
+    {
+        return $performance->play->type === 'comedy'
+            ? max($performance->audience - 30, 0) + (int)floor($performance->audience / 5)
+            : max($performance->audience - 30, 0);
+    }
+
+    public function totalVolumeCredits(stdClass $data): int
+    {
+        return (int)array_reduce($data->performances, function ($total, $performance) {
+            return $total + $performance->volumeCredits;
+        }, 0);
+    }
+
+    public function totalAmount(stdClass $data): int
+    {
+        return array_reduce($data->performances, function ($total, $performance) {
+            return $total + $performance->amount;
+        }, 0);
+    }
+}
--- php/src/StatementPrinter.php
+++ php/src/StatementPrinter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Theatrical;
 
-use Error;
 use NumberFormatter;
 use stdClass;
 
@@ -12,24 +11,7 @@ class StatementPrinter
 {
     public function statement(Invoice $invoice, array $plays): string
     {
-        return $this->renderPlainText($this->createStatementData($invoice, $plays));
-    }
-
-    private function createStatementData($invoice, $plays): stdClass
-    {
-        $statementData = new stdClass();
-        $statementData->customer = $invoice->customer;
-        $statementData->performances = array_map(function (Performance $performance) use ($plays) {
-            $result = clone $performance;
-            $result->play = clone $this->playFor($result, $plays);
-            $result->amount = $this->amountFor($result);
-            $result->volumeCredits = $this->volumeCreditsFor($result);
-            return $result;
-        }, $invoice->performances);
-        $statementData->totalAmount = $this->totalAmount($statementData);
-        $statementData->totalVolumeCredits = $this->totalVolumeCredits($statementData);
-
-        return $statementData;
+        return $this->renderPlainText((new CreateStatementData())->createStatementData($invoice, $plays));
     }
 
     private function renderPlainText(stdClass $data): string
@@ -47,59 +29,9 @@ class StatementPrinter
         return $result;
     }
 
-    protected function amountFor(Performance $performance): int
-    {
-        switch ($performance->play->type) {
-            case 'tragedy':
-                $result = 40000;
-                if ($performance->audience > 30) {
-                    $result += 1000 * ($performance->audience - 30);
-                }
-                break;
-
-            case 'comedy':
-                $result = 30000;
-                if ($performance->audience > 20) {
-                    $result += 10000 + 500 * ($performance->audience - 20);
-                }
-                $result += 300 * $performance->audience;
-                break;
-
-            default:
-                throw new Error("Unknown type: {$performance->play->type}");
-        }
-        return $result;
-    }
-
-    private function playFor(Performance $performance, array $plays): Play
-    {
-        return $plays[$performance->playID];
-    }
-
-    public function volumeCreditsFor(Performance $performance): int
-    {
-        return $performance->play->type === 'comedy'
-            ? max($performance->audience - 30, 0) + (int)floor($performance->audience / 5)
-            : max($performance->audience - 30, 0);
-    }
-
     public function usd(int $number): string
     {
         return (new NumberFormatter('en_US', NumberFormatter::CURRENCY))
             ->formatCurrency($number / 100, 'USD');
-    }
-
-    public function totalVolumeCredits(stdClass $data): int
-    {
-        return (int)array_reduce($data->performances, function ($total, $performance) {
-            return $total + $performance->volumeCredits;
-        }, 0);
-    }
-
-    public function totalAmount(stdClass $data): int
-    {
-        return array_reduce($data->performances, function ($total, $performance) {
-            return $total + $performance->amount;
-        }, 0);
     }
 }

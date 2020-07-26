89269477e781f838f410f4b5218ab6136fa01b70
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-07-26 18:43:27 +0100

    Split to own Class (file)

--- php/src/StatementPrinter.php
+++ php/src/StatementPrinter.php
@@ -14,6 +14,11 @@ class StatementPrinter
         return $this->renderPlainText((new CreateStatementData())->createStatementData($invoice, $plays));
     }
 
+    public function htmlStatement(Invoice $invoice, array $plays): string
+    {
+        return $this->renderHtml((new CreateStatementData())->createStatementData($invoice, $plays));
+    }
+
     private function renderPlainText(stdClass $data): string
     {
         $result = "Statement for {$data->customer}\n";
@@ -29,9 +34,29 @@ class StatementPrinter
         return $result;
     }
 
-    public function usd(int $number): string
+    private function usd(int $number): string
     {
         return (new NumberFormatter('en_US', NumberFormatter::CURRENCY))
             ->formatCurrency($number / 100, 'USD');
+    }
+
+    private function renderHtml(stdClass $data): string
+    {
+        $result = "<h1>Statement for {$data->customer}</h1>";
+        $result .= "<table>";
+        $result .= "  <tr><th>play</th><th>seats</th><th>cost</th></tr>";
+        /** @var Performance $performance */
+        foreach ($data->performances as $performance) {
+            // print line for this order
+            $result .= "  <tr>";
+            $result .= "    <td>{$performance->play->name}</td>";
+            $result .= "    <td>({$performance->audience} seats)</td>";
+            $result .= "    <td>{$this->usd($performance->amount)}</td>";
+            $result .= "  </tr>";
+        }
+        $result .= "</table>";
+        $result .= "<p>Amount owed is {$this->usd($data->totalAmount)}</p>";
+        $result .= "<p>You earned {$data->totalVolumeCredits} credits</p>";
+        return $result;
     }
 }
